2662bcbce04738d452b597f8f522bfa86d263311
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-29 18:21:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
     squared_numbers_array = []
     array.each do |numbers|
-    numbers_squared = numbers * numbers
+    numbers_squared = numbers ** 2
   end
   return squared_numbers_array
 end
